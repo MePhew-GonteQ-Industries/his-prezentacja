694e4b31f44cf7d7249641496a5b671fc24812ef
biQte <patrykgonet@gmail.com>
2023-03-15 16:41:47 +0100

    Add web sockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,39 @@
 import "dotenv/config";
-import express from "express";
+import express, { json } from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
+import apiRouter from "./routes/router";
 
 const app = express();
 
+app.use(json());
+app.use("/api", apiRouter);
+
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket: Socket) => {
-  console.log("User connected");
-  socket.send("Hi there I`m socket server and you are connected");
+  console.log(`User with id: ${socket.id} connected`);
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log(`User with id: ${socket.id} disconnected`);
+  });
+
+  socket.on("message", (message: string) => {
+    console.log(message);
+    socket.broadcast.emit("message", message);
   });
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server started and is listening on port ${process.env.PORT}`);
-});
+server
+  .listen(process.env.PORT, () => {
+    console.log(`Server started and is listening on port ${process.env.PORT}`);
+  })
+  .on("error", (error) => {
+    console.log(error);
+  });
