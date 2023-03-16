250932102b0d0297fe056bd78ba4b5bd6224f6c6
biQte <patrykgonet@gmail.com>
2023-03-16 17:45:44 +0100

    Add getting users location

--- ui/src/views/GameViewSpectator.vue
+++ ui/src/views/GameViewSpectator.vue
@@ -1,5 +1,36 @@
 <script setup lang="ts">
+import { useMainStore } from '@/stores/main';
+import { io } from 'socket.io-client';
+import { storeToRefs } from 'pinia';
+import { useMessage } from 'naive-ui';
 
+const mainStore = useMainStore();
+
+const messageBox = useMessage();
+
+const { socket } = storeToRefs(mainStore);
+
+if (!socket.value) {
+    socket.value = io("http://localhost:4000");
+}
+
+socket.value.emit('joinRoom', 'spectators');
+
+socket.value.on('message', (message: string) => {
+    messageBox.info(message);
+});
+
+socket.value.on('posX', (posX: number, user: string) => {
+    console.log(`PosX: ${posX} for user: ${user}`);
+});
+
+socket.value.on('posY', (posY: number, user: string) => {
+    console.log(`PosY: ${posY} for user: ${user}`);
+});
+
+socket.value.on('rotation', (rotation: number, user: string) => {
+    console.log(`Rotation: ${rotation} for user: ${user}`);
+});
 </script>
 
 <template>
@@ -8,5 +39,4 @@
     </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped lang="scss"></style>
